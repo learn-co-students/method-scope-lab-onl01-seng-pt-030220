70cb0e9c5933c75f5608e5f25bcd8195a087048d
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-14 23:00:54 +0000

    Done.

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,11 @@
-def catch_phrase
-  puts phrase
+
+
+def catch_phrase(argu = "It's-a me, Mario!" )
+   puts argu
+   # return argu
 end
+
+catch_phrase
+
+# you can put an argument that point to a string variable type but I amde it optional
+# by giving the method a default argument to put
